37eed99edbc732c326688871701a22ec0d8ffb15
williansmalque <williansmalque76@gmail.com>
2026-07-20 21:38:43 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 
 import { useState } from 'react';
-import type { Product } from '@/data/produtcs';
+import { products, type Product } from '@/data/produtcs';
 export default function Home() {
   // Estados UI
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
